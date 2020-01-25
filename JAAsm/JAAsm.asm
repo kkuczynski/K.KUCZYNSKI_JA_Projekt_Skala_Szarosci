a8846376b2138df33b238b341788e088ac316632
Krzysztof Kuczyński <krzykuc903@student.polsl.pl>
2020-01-25 16:10:50 +0100

    Przeprojektowanie na program orientowany obiektowo, bo robil sie balagan, naprawienie przesuniecia obrazu po przetworzeniu go, asm przetwarza tylko czesc obrazu

--- JAAsm/JAAsm.asm
+++ JAAsm/JAAsm.asm
@@ -1,9 +1,87 @@
-.code 
-
-MyProc1 proc
-add RCX, RDX
-mov RAX, RCX
-
+.DATA
+three REAL8 3.0
+.CODE
+DllEntry PROC hInstDLL:DWORD, reason:DWORD, reserved1:DWORD
+mov rax, 1
 ret
-MyProc1 endp
-end
+DllEntry ENDP
+
+MyProc proc tab:qword,wdth:qword,wdthbytes:qword,start:dword,theend:dword
+
+;inicjalizacja zmiennych
+mov tab, rcx
+mov wdth, rdx
+mov wdthbytes, r8
+mov start, r9d
+mov r10d, theend
+
+;obliczam ilosc zer
+mov r11,r8		;do r11 wpisuje wartosc znajdujaca sie w r8: wdthbytes
+mov rax, 3		;przygotowuje 3 
+mul rdx			;mnoze zawartosc rdx razy rax - czyli szerokosc * 3
+mov r12, rax	;do r12 wpisuje wynik, ktory jest w rax
+sub r11,r12		;od r11 odejmuje r12 , byteswdth - 3*wdth
+
+mov rdx, wdth	;do rdx wpisuje szerokosc
+
+mov rsi, rcx	;wskaznik mapy obrazu, punkt poczatkowy obrazu
+mov rax, 0		;zeruje rax
+mov eax, r9d	;do eax wpisuje start - wysokosc poczatku dla danego watku
+mul r8			;mnoz wdthbytes
+mov r12, rax	;do r12 wpisujemy rax- pole
+add rsi,r12		;do rsi dodaje przesuniecie - przesuwanie wskaznika
+
+mov rdx, wdth   ;do rdx wpisuje szerokosc
+
+
+;pêtla for i
+mov r15d, r9d	;int i = start
+_fori:
+cmp r15d, r10d	;i<theend
+jae _end_fori
+
+	;pêtla for j
+		mov r14,0	;int j=0
+		_forj:
+		cmp r14, rdx	;j<wdth
+		jae _end_forj
+
+				movzx eax, byte ptr[rsi]	;pobieram pierwszy kolor do eax, niebieski
+		;wektorowa instrukcja
+				cvtsi2sd xmm1,eax			;zamiana 32bitowej wartosci ca³kowitoliczbowej do 64bajtowej zmiennoprzecinkowej czyli przekazanie wartosci koloru blue do xmm1
+				movzx eax, byte ptr[rsi+1]  ;pobieram drugi kolor do eax, zielony
+		;wektorowa isntrukcja
+				cvtsi2sd xmm2,eax			;zamiana 32bitowej wartosci ca³kowitoliczbowej do 64bajtowej zmiennoprzecinkowej
+				movzx eax, byte ptr[rsi+2]  ;pobieram trzeci kolor, czerwony
+		;wektorowa instrukcja
+				cvtsi2sd xmm3,eax			;zamiana 32bitowej wartosci ca³kowitoliczbowej do 64bajtowej zmiennoprzecinkowej
+
+			;wektorowa isntrukcja
+			;obliczanie sredniej kolorow
+				addsd xmm1,xmm2				;dodanie do xmm1 wartosci z xmm2 (B+G)
+				addsd xmm1,xmm3				;dodanie do poprzednio uzyskanej xmm1 wartosci z xmm3 ((B+G)+R)
+				divsd xmm1,three			;dzielenie xmm1 przez 3 - czyli wyliczanie sredniej, by uzyskac kolor szarosci
+
+			;wektorowa instrukcja
+				cvttsd2si eax,xmm1		 ;zamiana 64bajtowej wartosci zmienorzecinkowej do 32bitowej ca³kowitoliczbowej, odwrotny proces do tego co wyzej
+				mov byte ptr[rsi],al	 ;zapisanie nowego "koloru" w miejsce skad byl pobierany, niebieski
+				mov byte ptr[rsi+1],al	 ;zapisanie nowego "koloru" w miejsce skad byl pobierany, zielony
+				mov byte ptr[rsi+2],al	 ;zapisanie nowego "koloru" w miejsce skad byl pobierany, czerwony
+
+		add rsi,3		;przejscie do kolejnego pixela
+		inc r14			;inkrementacja j++
+		jmp _forj		;skok do petli j
+		_end_forj:
+		_retfori:
+		
+
+add rsi, r11	;przesuniecie wskaznika o ilosc zer, ktore sa konuc, wyliczonych wczesniej
+inc r15d		;inkrementacja licznika i++
+jmp _fori		;skok do petli fori
+_end_fori:      ;jak sie petla konczy to return
+ret
+
+MyProc endp
+
+END
+
--- JAProj/JAProj.cpp
+++ JAProj/JAProj.cpp
@@ -9,13 +9,13 @@ using namespace std;
 #include <thread>
 #include <string>
 #include <vector>
-void MyProc2(unsigned char* bmptab, int hgth, int wdth, int threads, int index);
+void MyProc2(unsigned char* tab, int wdth, int wdth_in_bytes, int start, int end);
 
-int CPP(unsigned char* data, int hght, int wdth, int threads, int index)
+int CPP(unsigned char* tab, int wdth, int wdth_in_bytes, int start, int end)
 {
 	HINSTANCE hGetProcIDDLL = LoadLibrary("JACpp.dll");
-	FARPROC lpfnGetProcessID = GetProcAddress(HMODULE (hGetProcIDDLL), "MyProc2");
-	typedef void(__cdecl* pICFUNC)(unsigned char*, int, int, int, int);
+	FARPROC lpfnGetProcessID = GetProcAddress(HMODULE(hGetProcIDDLL), "MyProc2");
+	typedef void(__cdecl * pICFUNC)(unsigned char*, int, int, int, int);
 	int error = GetLastError();
 	DWORD WINAPI FormatMessage(
 		DWORD dwFlags,
@@ -28,11 +28,295 @@ int CPP(unsigned char* data, int hght, int wdth, int threads, int index)
 	);
 	pICFUNC function;
 	function = pICFUNC(lpfnGetProcessID);
-	function(data, hght, wdth, threads, index);
-
+	function(tab, wdth, wdth_in_bytes, start, end);
 	FreeLibrary(hGetProcIDDLL);
 	return 0;
 }
+
+int ASM(unsigned char* tab, int wdth, int wdth_in_bytes, int start, int end)
+{
+	HINSTANCE hGetProcIDDLL = LoadLibrary("JAAsm.dll");
+	FARPROC lpfnGetProcessID = GetProcAddress(HMODULE(hGetProcIDDLL), "MyProc");
+	typedef void(__cdecl * pICFUNC)(unsigned char*, int, int, int, int);
+	int error = GetLastError();
+	DWORD WINAPI FormatMessage(
+		DWORD dwFlags,
+		LPCVOID lpSource,
+		DWORD dwMessageId,
+		DWORD dwLanguageId,
+		LPTSTR lpBuffer,
+		DWORD nSize,
+		va_list * Arguments
+	);
+	pICFUNC function;
+	function = pICFUNC(lpfnGetProcessID);
+	function(tab, wdth, wdth_in_bytes, start, end);
+	FreeLibrary(hGetProcIDDLL);
+	return 0;
+}
+class Byte {
+public:
+	Byte() //konstruktor domyslny, dla pojedynczego bajtu
+	{
+		bitcount = 8;
+	}
+	Byte(int _bitcount) //konstruktor dla sklejanych bajtow
+	{
+		bitcount = _bitcount;
+	}
+	int val;
+	char tab[32]; 
+
+	int bitcount;
+
+	void to_dec() //zmiana na wartosc dziesietna
+	{
+		unsigned long _val = 1;
+		this->val = 0;
+
+		for (int i = 0; i < this->bitcount; i++)
+		{
+			if (this->tab[this->bitcount - i - 1] == '1')
+				this->val += _val;
+			_val = 2 * _val;
+		}
+		return;
+	}
+
+
+	void to_bin() //zmiana na wartosc binarna
+	{
+		int _val = this->val;
+		
+		for (int i = 0; i < this->bitcount; i++)
+		{
+			this->tab[this->bitcount - i - 1] = 48 + (_val % 2);
+			_val = _val / 2;
+
+		}
+		return;
+	}
+
+	void to_32bit(Byte b1, Byte b2, Byte b3, Byte b4) //Sklejenie czterech bajtów
+	{
+		b4.to_bin();
+		b3.to_bin();
+		b2.to_bin();
+		b1.to_bin();
+		for (int i = 0; i < 8; i++)
+		{
+			this->tab[i] = b4.tab[i];
+		}
+		for (int i = 8; i < 16; i++)
+		{
+			this->tab[i] = b3.tab[i-8];
+		}
+		for (int i = 16; i < 24; i++)
+		{
+			this->tab[i] = b2.tab[i-16];
+		}
+		for (int i = 24; i < 32; i++)
+		{
+			this->tab[i] = b1.tab[i-24];
+		}
+		
+		
+	}
+};
+class BMP {
+public:
+	unsigned char* tab;
+	unsigned char* newtab;
+	FILE* file;
+	FILE* newfile;
+	int size;
+	int padding;
+	int hght;
+	int wdth;
+	int wdth_in_bytes;
+	int address;
+
+	bool loadfile(const char* name)
+	{
+		int loaded;
+		//const char* name = "test.bmp";
+		this->file = fopen(name, "rb");
+		if (!this->file) //przy braku pliku zwraca false
+			return false;
+
+		
+		
+		Byte* b1 = new Byte(); //Tworzenie czterech bajtow, do ktorych zapisywane beda kolejno dane z pliku
+		Byte* b2 = new Byte();
+		Byte* b3 = new Byte();
+		Byte* b4 = new Byte();
+
+		fseek(this->file, 10, SEEK_SET); //Zczytanie adresu danych
+		
+		loaded = getc(this->file);
+		b1->val = loaded;
+		loaded = getc(this->file);
+		b2->val = loaded;
+		loaded = getc(this->file);
+		b3->val = loaded;
+		loaded = getc(this->file);
+		b4->val = loaded;
+
+		
+		Byte* groupped = new Byte(32); //Obiekt przechowujacy cztery sklejone bajty
+		groupped->to_32bit(*b1, *b2, *b3, *b4); //sklejenie bajtow
+		groupped->to_dec(); //konwersja na system dziesietny
+
+		this->address = groupped->val; //Przypisanie zaladowanej wartosci do adresu danych
+		
+		fseek(this->file, 18, SEEK_SET); //Zczytanie szerokosci 
+
+		loaded = getc(this->file);
+		b1->val = loaded;
+		loaded = getc(this->file);
+		b2->val = loaded;
+		loaded = getc(this->file);
+		b3->val = loaded;
+		loaded = getc(this->file);
+		b4->val = loaded;
+
+		groupped->to_32bit(*b1, *b2, *b3, *b4);
+		groupped->to_dec();
+
+		
+		this->wdth = groupped->val; //Przypisanie zaladowanej wartosci do szerokosci 
+
+		
+		fseek(this->file, 34, SEEK_SET); //Zczytanie wielkosci danych
+
+		loaded = getc(this->file);
+		b1->val = loaded;
+		loaded = getc(this->file);
+		b2->val = loaded;
+		loaded = getc(this->file);
+		b3->val = loaded;
+		loaded = getc(this->file);
+		b4->val = loaded;
+
+		
+		groupped->to_32bit(*b1, *b2, *b3, *b4);
+		groupped->to_dec();
+
+		
+		this->size = groupped->val; //Przypisanie wartosci do rozmiaru danych
+
+		getpadding();
+
+		fseek(this->file, this->address, SEEK_SET);
+		tab = new unsigned char[this->size + this->wdth_in_bytes];
+		for (int i = 0; i < this->size; i++)
+		{
+			tab[i] = getc(this->file);
+		}
+		
+		for (int i = this->size; i < this->size + this->wdth_in_bytes; ++i)
+		{
+			tab[i] = tab[i - this->wdth_in_bytes];
+		}
+		return true;
+	}
+	void getpadding()
+	{
+		this->padding = (4 - ((this->wdth * 3) % 4)) % 4; 
+		this->wdth_in_bytes = this->wdth * 3 + padding; //obliczenie szerokosci w bajtach po wyrownaniu do 4
+		this->hght = this->size / this->wdth_in_bytes;
+	}
+	void write_newtab()	//przygotowanie tablicy newtab
+	{
+		newtab = new unsigned char[this->wdth * 4 * (this->hght + 1)];
+		int h = 0;
+		for (int i = 0; i <= hght; i++)
+		{
+			int _hght = i * wdth * 4;
+			for (int j = 0; j < wdth; j++)
+			{
+				int _wdth = j * 4;
+				newtab[_hght + _wdth] = tab[h++];
+				newtab[_hght + _wdth + 1] = tab[h++];
+				newtab[_hght + _wdth + 2] = tab[h++];
+				newtab[_hght + _wdth + 3] = 0;
+			}
+			h += padding;
+		}
+	}
+	void write_tab()	//przygotowuje przetworzone dane do zapisu
+	{
+		int h = 0;
+		for (int i = 0; i <= hght; i++)
+		{
+			int Wys = i * wdth * 4;
+			for (int j = 0; j < wdth; j++)
+			{
+				int Szer = j * 4;
+				tab[h++] = newtab[Wys + Szer];
+				tab[h++] = newtab[Wys + Szer + 1];
+				tab[h++] = newtab[Wys + Szer + 2];
+			}
+			h += padding;
+		}
+	}
+
+	void CPPThreads(int threads) //funkcja tworzaca wektor watkow z funkcjami biblioteki CPP
+	{
+		vector<thread> threads_vec(threads); //utworzenie wektora watkow
+		for (int i = 0; i < threads-1; i++)
+		{
+			threads_vec[i] = thread(CPP, this->newtab, wdth, wdth_in_bytes, int(floor(float(hght) / float(threads) * i)), int(ceil((float)hght / (float)threads) * (i + 1)));//wyliczenie poczatku i konca obszaru tab dla jednego watku
+		}
+		threads_vec[threads-1] = thread(CPP, this->newtab, wdth, wdth_in_bytes, int(floor(float(hght) / float(threads) * (threads-1))), int(((float)hght / (float)threads) * (threads)));//wyliczenie poczatku i konca obszaru tab dla jednego watku
+
+		for (int i = 0; i < threads; i++)
+		{
+			threads_vec[i].join();
+		}
+		
+	}
+
+	void ASMThreads(int threads)//funkcja tworzaca wektor watkow z funkcjami biblioteki asm
+	{
+		vector<thread> threads_vec(threads); //utworzenie wektora watkow
+		for (int i = 0; i < threads-1; i++)
+		{
+			threads_vec[i] = thread(ASM, this->newtab, wdth, wdth_in_bytes, int(floor(float(hght) / float(threads) * i)), int(ceil((float)hght / (float)threads) * (i + 1)));//wyliczenie poczatku i konca obszaru tab dla jednego watku
+		}
+		threads_vec[threads - 1] = thread(ASM, this->newtab, wdth, wdth_in_bytes, int(floor(float(hght) / float(threads) * (threads-1))), int(((float)hght / (float)threads) * (threads)));
+		for (int i = 0; i < threads; i++)
+		{
+			threads_vec[i].join();
+		}
+
+	}
+
+	void savefile(const char* newname)
+	{
+		int val;
+
+		newfile = fopen(newname, "wb");		//Tworzenie nowej mapy bitowej
+		fseek(file, 0L, SEEK_SET);				//Ustawienie na poczatku starej bitmapy w celu przepisania naglowka
+
+		for (int i = 0; i < this->address; ++i)	//Przepisanie naglowka
+		{
+			val = getc(file);
+			putc(val, newfile);
+		}
+
+		for (int i = 0; i < size; ++i)	//!Przepisuj kolejne bajty obszaru danych zmieniaj¹c ostatni bit ka¿dego koloru
+			putc(tab[i], newfile);
+
+		fclose(newfile);
+
+		return;
+	}
+	
+};
+
+
+
 /*void ASM(unsigned char* data, int hght, int wdth, int threads, int index)
 {
 	HINSTANCE hGetProcIDDLL = LoadLibrary("JAAsm.dll");
@@ -48,10 +332,11 @@ void emptyTest() //funkcja tymczasowa do wywolania w watkach
 }
 int main()
 {
+	BMP bmp;
 	int threads = 0;
 	int input = 0;
 	char choice;
-	const char* name = "yoda.bmp";
+	const char* name = "test.bmp";
 	const char* newname = "graytest.bmp";
 	bool ifAsm = false;
 	string flush = "";
@@ -84,18 +369,19 @@ int main()
 		cout << "Wybrana biblioteka: c++\n";
 	}
 	
-	FILE* f = fopen(name, "rb");
-	unsigned char header[54];
-	fread(header, sizeof(unsigned char), 54, f); //zczytanie naglowka pliku bmp
+	
+	//FILE* f = fopen(name, "rb");
+	//unsigned char header[54];
+	//fread(header, sizeof(unsigned char), 54, f); //zczytanie naglowka pliku bmp
 
 	//wydobycie szerokosci i wysokosci z naglowka
-	int wdth = *(int*)&header[18];
-	int hght = *(int*)&header[22];
+	//int wdth = *(int*)&header[18];
+	//int hght = *(int*)&header[22];
 
-	int size = 3 * wdth * hght; // rozmiar tablicy pikseli (b, g, r)
-	unsigned char* data = new unsigned char[size]; //alokowanie 3 bajtow na piksel
-	fread(data, sizeof(unsigned char), size, f); //zczytanie wartosci kolorow pikseli
-	fclose(f);
+	//int size = 3 * wdth * hght; // rozmiar tablicy pikseli (b, g, r)
+	//unsigned char* data = new unsigned char[size]; //alokowanie 3 bajtow na piksel
+	//fread(data, sizeof(unsigned char), size, f); //zczytanie wartosci kolorow pikseli
+	//fclose(f);
 	//for (int i = 0; i < size/100;)
 	//{
 	//	cout << (float)data[i] << " " << (float)data[i + 1] << " " << (float)data[i + 2] << "\n";
@@ -103,25 +389,40 @@ int main()
 	//}
 	if (ifAsm)//korzystaj z biblioteki asm, jesli uzytkownik tego chce
 	{
-		
+		if (bmp.loadfile(name))
+		{
+			bmp.write_newtab();
+			bmp.ASMThreads(threads);
+			bmp.write_tab();
+			bmp.savefile(newname);
+		}
 	}
 	else
 	{
-		vector<thread>threadsVec(threads);
-		for (int i = 0; i < threads; i++)
+		if (bmp.loadfile(name))
 		{
-			threadsVec[i] = thread(CPP,data, hght, wdth, threads, i+1);
-			
-			threadsVec[i].join();
+			bmp.write_newtab();
+			bmp.CPPThreads(threads);
+			bmp.write_tab();
+			bmp.savefile(newname);
 		}
+		//vector<thread>threadsVec(threads);
+		//clock_t start = clock();
+		//for (int i = 0; i < threads; i++)
+		//{
+		//	threadsVec[i] = thread(CPP,data, hght, wdth, threads, i+1);
+			
+		//	threadsVec[i].join();
+		//}
+		//cout << "Watki: " << threads << " czas: " << (float)(clock() - start)/CLOCKS_PER_SEC;
 	}
-	FILE* newf = fopen(newname, "w"); //utworzenie pustego pliku
-	fclose(newf);
-	newf = fopen(newname, "a");  //dopisanie do pliku nowej bitmapy
+	//FILE* newf = fopen(newname, "w"); //utworzenie pustego pliku
+	//fclose(newf);
+	//newf = fopen(newname, "a");  //dopisanie do pliku nowej bitmapy
 	
-	fwrite(header, sizeof(unsigned char), 54, newf); //dodanie naglowka do nowego pliku bmp
-	fwrite(data, sizeof(unsigned char), size, newf); //dopisanie wartosci pikseli do pliku bmp
-	fclose(newf);
+	//fwrite(header, sizeof(unsigned char), 54, newf); //dodanie naglowka do nowego pliku bmp
+	//fwrite(data, sizeof(unsigned char), size, newf); //dopisanie wartosci pikseli do pliku bmp
+	//fclose(newf);
 	//cout << "\n\nNOWE\n\n";
 	//for (int i = 0; i < size / 100;)
 	//{
